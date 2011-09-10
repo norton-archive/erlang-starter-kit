c62c5cf5941b5e52e1f2036b2fe0912ae746e282
Joseph Wayne Norton <norton@alum.mit.edu>
2011-09-11 00:19:29 +0900

    Disable eqcmini

--- init.erl
+++ init.erl
@@ -2,7 +2,7 @@
 
 true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/edown/ebin").
 true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/eper/ebin").
-true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/eqc/ebin").
+%%true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/eqc/ebin").
 true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/meck/ebin").
 true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/proper/ebin").
 true = code:add_pathz(os:getenv("HOME")++"/.erlang.d/lib/qc/ebin").
